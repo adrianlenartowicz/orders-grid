ab28a9963ac6a2f4ca922f1bbf4fb8150048a41e
Adrian <adrian@lenartowicz.net>
2026-02-17 23:07:00 +0100

    feat: load orders and temporarily render orders symbols

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { OrdersApiService } from './services/orders-api.service';
+import { Order } from './models/order.model';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('orders-grid');
+export class App implements OnInit {
+  private api = inject(OrdersApiService);
+
+  orders: Order[] = [];
+
+  ngOnInit() {
+    this.api.getOrders().subscribe(orders => {
+      this.orders = orders;
+      console.log(this.orders)
+    })
+  }
 }
