b741b514678595545cae30002e4f908c4fd215ee
Adrian <adrian@lenartowicz.net>
2026-02-18 00:03:22 +0100

    feat: render grid with grouped orders

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,43 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { OrdersApiService } from './services/orders-api.service';
 import { Order } from './models/order.model';
+import { AgGridAngular } from 'ag-grid-angular'
+import type { ColDef } from 'ag-grid-community';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [AgGridAngular],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App implements OnInit {
   private api = inject(OrdersApiService);
 
-  orders: Order[] = [];
+  orders = signal<Order[]>([]);
+
+  colDef: ColDef[] = [
+    {
+      field: 'symbol',
+      rowGroup: true,
+      hide: true
+    },
+    {
+      field: 'openPrice',
+      aggFunc: 'avg'
+    },
+    {
+      field: 'swap',
+      aggFunc: 'sum'
+    },
+    {
+      field: 'size',
+      aggFunc: 'sum'
+    }
+  ]
 
   ngOnInit() {
-    this.api.getOrders().subscribe(orders => {
-      this.orders = orders;
-      console.log(this.orders)
+    this.api.getOrders().subscribe(data => {
+      this.orders.set(data);
     })
   }
 }
